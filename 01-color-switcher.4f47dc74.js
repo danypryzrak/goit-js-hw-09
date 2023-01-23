6562516aab4a093a480cf18b7ce096e330ff85f3
danypryzrak <116654935+danypryzrak@users.noreply.github.com>
2023-01-23 16:08:35 +0000

    Deploying to gh-pages from @ danypryzrak/goit-js-hw-09@c8207a67beefc7a0b2d7934873963118cb1638bd 🚀

---
+++ 01-color-switcher.4f47dc74.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),r=document.querySelector("button[data-stop]");let o=null;e.addEventListener("click",(()=>{o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.setAttribute("disabled","true")})),r.addEventListener("click",(()=>{clearInterval(o),e.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.4f47dc74.js.map
